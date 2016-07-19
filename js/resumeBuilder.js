5d6491d4c1e193692bd023df9fa24e6b1a0b5564
jcpleite <jcpleite@gmail.com>
2016-07-19 14:03:16 -0300

    Add main, var, replace, array, name and role.     prepend and append method     quiz exercice

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,53 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ // $("#main").append(["José Carlos Portugal"]);
+
+ 
+ /* Testes
+ var email = "jcpleite@hotmail.com";
+
+ var newEmail = email.replace("hotmail", "gmail");
+
+ console.log(email);
+
+ console.log(newEmail);
+
+ //Replace 
+
+ var awesomeThoughts = 
+ 	 	"I am José Carlos and I am AWESOME!";
+
+ var funThoughts = 
+ 		awesomeThoughts.replace("AWESOME","FUN");
+
+ $("#main").append(funThoughts);
+
+ console.log(awesomeThoughts); 
+ 
+
+ //Array 
+
+ var skills = ["awesomeness", "programming", "teaching", "JS"];
+ 
+ $("#main").append(skills);
+
+ $("#main").append(skills[0]);
+
+ $("#main").append(skills.length);
+ */
+
+/* Code for my resume */
+
+var name = "José Carlos Portugal";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Product Manager, CTO";
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+
