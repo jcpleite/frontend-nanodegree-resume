5bf4d65ac012a583ad2164586db1a50d97e8727e
jcpleite <jcpleite@gmail.com>
2016-07-21 19:19:22 -0300

    Add bio, education, jobs

--- js/quiz.js
+++ js/quiz.js
@@ -14,4 +14,119 @@ var udacityizer = function(s) {
 // Did your code work? The line below will tell you!
 console.log(udacityizer(s));
 
+Array
+
+function incrementLastArrayElement(_array)  {
+    var newArray = [];
+    newArray = _array.slice(0);
+    var lastNumber = newArray.pop();
+    newArray.push(lastNumber + 1);
+    return newArray;
+}
+
+Array Strings
+
+function incrementLastArrayElement(_array)  {
+    var newArray = [];
+    newArray = _array.slice(0);
+    var lastNumber = newArray.pop();
+    newArray.push(lastNumber + 1);
+    return newArray;
+}
 */
+
+ // $("#main").append(["José Carlos Portugal"]);
+
+ 
+ /* Testes
+ var email = "jcpleite@hotmail.com";
+
+ var newEmail = email.replace("hotmail", "gmail");
+
+ console.log(email);
+
+ console.log(newEmail);
+
+ //Replace 
+
+ var awesomeThoughts = 
+ 	 	"I am José Carlos and I am AWESOME!";
+
+ var funThoughts = 
+ 		awesomeThoughts.replace("AWESOME","FUN");
+
+ $("#main").append(funThoughts);
+
+ console.log(awesomeThoughts); 
+ 
+*/
+ //Array 
+/*
+ var skills = ["awesomeness", "programming", "teaching", "JS"];
+ 
+ $("#main").append(skills);
+
+ $("#main").append(skills[0]);
+
+ $("#main").append(skills.length); 
+*/
+/*
+ //Object
+
+ var bio = {
+	 "name": "José Carlos Portugal",
+	 "role": "Product Manager - CTO",
+	 "contacts": {
+	 	"mobile": "+55 11 991942063",
+	 	"email": "jcpleite@gmail.com",
+	 	"github": "jcpleite",
+	 	"twitter": "@jcpfleite",
+	 	"location": "São Paulo",
+	 }, 
+	 "welcomeMessage" : "Engineer, Executive MBA for BSP - Business School São Paulo, extension in Suffolk University in Leadership, experience in the Internet Industry.",
+	 "skills": ["Agile", "Programming", "Git and Github", "JS"],
+	 "bioPic" : "images/JC.jpg"
+	}
+
+ $("#main").append(bio.name);
+ $("#main").append(bio.role);
+ $("#main").append(bio.bioPic);
+ $("#main").append(bio.age);
+ $("#main").append(bio.welcomeMessage);
+ $("#main").append(bio.skills);
+ */
+/*
+ //Object Sintex
+
+ var work = {};
+ work.position = "CTO";
+ work.employer = "Evino";
+ work.months = 5;
+
+ var education = {};
+ education["name"] = "BSP Business School São Paulo";
+ education["years"] = "2013-2015";
+ education["city"] = "São Paulo, SP, Brasil";
+
+ $("#main").append(work["position"]);
+ $("#main").append(education.name);
+ */
+/*
+ //JSON
+
+ var education = {
+	"Formação acadêmica": [{
+		"instituição de ensino": "BSP - Business School São Paulo",
+		"localidade": "São Paulo, SP",
+		"formação": "Executive Master of Business Administration (EMBA)",
+		"ano de formação": 2015
+	}, {
+		"instituição de ensino": "Centro Universitário Nove de Julho",
+		"localidade": "São Paulo, SP",
+		"formação": "Engineer, Production Engineering",
+		"ano de formação": 2010
+	}]
+}
+*/
+
+
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,53 +1,53 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//Code for my resume 
 
- // $("#main").append(["José Carlos Portugal"]);
+var bio = {
+		"name": "José Carlos Portugal",
+		"role": "Product Manager, CTO",
+		"contacts": {
+			"mobile": "+55 11 99194-2063",
+			"email": "jcpleite@gmail.com",
+			"github": "jcpleite",
+			"location": "São Paulo"
+		},
+		"resume": "Engineer, Executive MBA for BSP - Business School São Paulo, extension in Suffolk University in Leadership in the Global Economy with more than 8 years experience in the Internet Industry in companies like Mercadolivre, Walmart and 5 years experience with Salesforce.com.",
+		"bioPic": "images/jc.jpg"
+		},
 
- 
- /* Testes
- var email = "jcpleite@hotmail.com";
+var education = {
+		"Formação acadêmica": [{
+			"instituição de ensino": "BSP - Business School São Paulo",
+			"localidade": "São Paulo, SP",
+			"formação": "Executive Master of Business Administration (EMBA)",
+			"ano de formação": 2015
+		}, {
+			"instituição de ensino": "Centro Universitário Nove de Julho",
+			"localidade": "São Paulo, SP",
+			"formação": "Engineer, Production Engineering",
+			"ano de formação": 2010
+		}]
+}
 
- var newEmail = email.replace("hotmail", "gmail");
-
- console.log(email);
-
- console.log(newEmail);
-
- //Replace 
-
- var awesomeThoughts = 
- 	 	"I am José Carlos and I am AWESOME!";
-
- var funThoughts = 
- 		awesomeThoughts.replace("AWESOME","FUN");
-
- $("#main").append(funThoughts);
-
- console.log(awesomeThoughts); 
- 
-
- //Array 
-
- var skills = ["awesomeness", "programming", "teaching", "JS"];
- 
- $("#main").append(skills);
-
- $("#main").append(skills[0]);
-
- $("#main").append(skills.length);
- */
-
-/* Code for my resume */
-
-var name = "José Carlos Portugal";
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var role = "Product Manager, CTO";
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedName);
-$("#header").append(formattedRole);
-
+var work = {
+	"jobs": [{
+		"nome da empresa": "Evino",
+		"position": "CTO - Chief Technology Officer",
+		"period": "September 2015 - January 2016",
+		"Description": " Key Responsibilities: Agile Culture (Kanban and Scrum). Product Specialist, Product Culture (Strategy + Business, Definition of quarter backlog for development)",
+		"Technical Knowledge": "Mobile Development with Swift, Knowledge PHP, Knowledge in product components (SOLR, Memcached, MySQL and Couchbase), Knowledge Solutions Architecture, Knowledge of Cloud Computing (AWS and Openstack).",
+		"Main Achievements": "Launch the first Brazil's wine buying app for IOS (Q4-2015) - Search for Evino on App Store, Implementation of new tax ICMS and IPI (Brazilian taxes) relative PEC 197/2012 constitutional amendment 87/2015 (http://www.planalto.gov.br/ccivil_03/Constituicao/Emendas/Emc/emc87.htm), Some products features and others features that were ready and not visible before adopting agile methodology."
+	}, {
+		"nome da empresa": "Walmart.com (Walmart eCommerce Brasil)",
+		"position": "Software Engineering Coordinator- E-Commerce Product",
+		"period": "October 2014 - September 2015",
+		"Description": " Key Responsibilities: Product Manager for ECommerce platforms in LATAM websites, Responsible for Products on MarketPlace Brazil, Outsourced development team coordination, Internal development team coordination.",
+		"Technical Knowledge": "Knowledge .NET and PHP, Knowledge in product components (Microsoft commerce server), Knowledge Solutions Architecture, Knowledge of Openstack.",
+		"Main Achievements": "Hot site for Cybermonday Argentina generating major revenue for the company (2014), Migration  superchangomas site for the local architecture providing greater availability, Migration with improvements than walmartonline.com.ar site for the local Brazil infrastructure and architecture providing greater availability, Site for HotSale Argentina generating major revenue for the company (2015)."
+	}, {
+		"nome da empresa": "Mercadolivre.com",
+		"position": "Project Leader at Product Development",
+		"period": "September 2008 - September 2014",
+		"Description": " Key Responsibilities: Product Owner, Scrum methodology, 5 years experience with Salesforce.com, Applying the concepts of UX design interfaces to internal systems and portal of contact for users of the platform, Project Management: Scope, Resources and Schedule, Lead IT projects for Customer Service and Fraud Prevention for three different countries, Strong interaction and coordination of different teams: IT, Product, Design, and other business units. Align stakeholders’ expectations in cross projects, Support and allign different areas and countries: Strategic Analysis; handling of BI tool, key metrics, identification of opportunities, fostes communication, Raising Net Promoter Scoring, Excellent relationship with other areas, Develop area. Build excellent professional and human team in 2 different countries, Development professionals who hold important positions in other areas.",
+		"Technical Knowledge": "Knowledge in PHP, Grails, Java, Ruby, Groove, Apex and REST API, , Knowledge in product components (Jboss, NodeJS, SOLR, Memcached, MySQL), Knowledge Solutions Architecture, Web Development with HTML, CSS and JavaScript, Testing tools Selenium Webdriver, Selenium ide.",
+		"Main Achievements": "Migration the customer experience service platform in 8 months to Salesforce.com using the Mercadolibre APIS with custom apex pages, workflows and triggers, Lead IT projects. Among the most important: Customer Support Portal, Migration CRM to SalesForce, Operational Tools, Fraud prevention, Users’ verification, Payment Accreditation, Credit Card Payments, Conflict Management, User Complaint, Facebook Connect, Mercado Pago bank resumen reconciliation account, MercadoAds (Advertisement) client treatment channel, and chargebacks conciliation, Tax incentives for R&D (Lei do bem), generating a tax savings on P&L company."
+	}]
+}
